47f60e166ccf899d8d8641c9b191d79c444db748
Paolo Tiu <paolotiu17@gmail.com>
2021-07-13 19:27:06 +0800

    fix docs build

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -41,34 +41,9 @@ module.exports = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/',
             },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+
             {
               label: 'GitHub',
               href: 'https://github.com/paolotiu/paymongo',
